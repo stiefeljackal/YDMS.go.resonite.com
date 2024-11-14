46869a05382f215bc2268971533c42ec3ed92423
Richard Fox <richard.fox@ahref.co.uk>
2024-11-13 16:48:50 -0800

    fix(routes): simplify everything

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-router.get('/record/:ownerId/:recordId', handleRecord);
-router.get('/session/:sessionId', handleSession);
+router.get('/record/:ownerId/:recordId', (req,res, next) => handleRecord);
+router.get('/session/:sessionId', (req,res, next) => handleSession);
 
-async function handleSession(req, res, next) {
-  var apiResponse = await fetch("https://api.resonite.com/sessions/" + req.params.sessionId);
-  if (!apiResponse.ok) {
-    return createError(apiResponse.status);
+function getUrl(type, req) {
+  switch (type) {
+    case "world":
+      return `https://api.resonite.com/users/${req.params.ownerId}/records/${req.params.recordId}/`;
+    case "session":
+      return "https://api.resonite.com/sessions/" + req.params.sessionId;
+    default:
+      throw new Error("Unknown url type");
   }
-  var json = await apiResponse.json();
-
-  json = preProcess(json);
-
-  res.status(200).render('session', json);
 }
 
-async function handleRecord(req, res, next) {
-  var url = `https://api.resonite.com/users/${req.params.ownerId}/records/${req.params.recordId}/`;
-  var apiResponse = await fetch(url);
+async function handle(type, req, res, next) {
+  var apiResponse = await fetch(getUrl(type, req));
   if (!apiResponse.ok) {
     return next(createError(apiResponse.status));
   }
+
   var json = await apiResponse.json();
-  if (json.recordType !== "world") {
-    return next(createError(400));
+
+  if (type ==="world" && json.recordType !== "world") {
+    return next(createError(400, "go.resonite.com only works for Session and world link."));
   }
 
-  res.status(200).render('session', json);
+  json = preProcess(json);
+
+  res.status(200).render(type, json);
 }
 
 function preProcessName(name) {
@@ -43,6 +45,7 @@ function preProcessName(name) {
 }
 function preProcess(json) {
   json.name = preProcessName(json.name);
+  
   if (!json.thumbnailUrl || json.thumbnailUrl === "")
     json.thumbnailUrl = "/images/noThumbnail.png";
 
