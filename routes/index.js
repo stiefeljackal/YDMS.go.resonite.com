e924d1cccbf20ff0f380959599b187b962ce9de1
Richard Fox <richard.fox@ahref.co.uk>
2024-11-15 16:17:45 -0800

    fix(titles): sanitize titles for open graph

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,32 @@
 var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
+var DOMPurify = require("isomorphic-dompurify");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/record/:ownerId/:recordId', (req,res, next) => handle("world", req, res, next));
 router.get('/session/:sessionId', (req,res, next) => handle("session", req, res, next));
-
-router.get('/record/:ownerId/:recordId/json', (req,res, next) => handleJson("world", req, res, next));
 router.get('/session/:sessionId/json', (req,res, next) => handleJson("session", req, res, next));
+
+// Register world/ and record/
+for (const word of ["world", "record"]) {
+  router.get(`/${word}/:ownerId/:recordId`, (req,res, next) => handle("world", req, res, next));
+  router.get(`/${word}/:ownerId/:recordId/json`, (req,res, next) => handleJson("world", req, res, next));
+}
+
+var baseUrl = "https://api.resonite.com";
 
 function getUrl(type, req) {
   switch (type) {
     case "world":
-      return `https://api.resonite.com/users/${req.params.ownerId}/records/${req.params.recordId}/`;
+      return `${baseUrl}/users/${req.params.ownerId}/records/${req.params.recordId}/`;
     case "session":
-      return "https://api.resonite.com/sessions/" + req.params.sessionId;
+      return `${baseUrl}/sessions/` + req.params.sessionId;
     default:
-      throw new Error("Unknown url type");
+      throw new Error(`Unknown url type: ${type}`);
   }
 }
 
@@ -94,39 +100,48 @@ function getOpenGraphTitle(type) {
 function preProcessName(name) {
   const start = /<color="?(.+?)"?>/gi;
   const end = /<\/color>/gi
-  return name.replace(start, "<span style=\"color: $1 ;\">").replace(end, "</span>");
+  return name.replace(start, "<span style=\"color: $1;\">").replace(end, "</span>");
 }
 
-function preProcess(json, type) {
-  json.name = preProcessName(json.name);
-
+function preProcessSession(json) {
   if (!json.thumbnailUrl || json.thumbnailUrl === "")
   {
     json.thumbnailUrl = "/images/noThumbnail.png";
   }
 
+  json.description = `Host: ${json.hostUsername}.\n` +
+     `Users ${json.totalJoinedUsers}/${json.maxUsers}:${json.sessionUsers.map(user => user.username).join(", ")}.\n`+
+     `Version: ${json.appVersion}`;
+
+  return json;
+}
+
+function preProcessWorld(json) {
   if (json.thumbnailUri) {
     json.thumbnailUri = json.thumbnailUri.replace("resdb:///", "https://assets.resonite.com/").replace(".webp", "");
   } else {
     json.thumbnailUri = "/images/noThumbnail.png";
   }
 
-  json.description = generateDescription(json, type);
-
-  json.title = json.name;
-
   return json;
 }
 
-function generateDescription(json, type) {
-  if (type =="world") {
-    return json.description;
+function preProcess(json, type) {
+
+  // ensure page title
+  json.title = DOMPurify.sanitize(json.name);
+  json.name = preProcessName(json.name);
+
+  switch (type) {
+    case "session":
+      json = preProcessSession(json);
+      break;
+    case "world":
+      json = preProcessWorld(json);
+      break;
   }
-  if (type =="session") {
-    return `Host: ${json.hostUsername}.\n` +
-     `Users ${json.totalJoinedUsers}/${json.maxUsers}:${json.sessionUsers.map(user => user.username).join(", ")}.\n`+
-     `Version: ${json.appVersion}`;
-  }
+
+  return json;
 }
 
 module.exports = router;
