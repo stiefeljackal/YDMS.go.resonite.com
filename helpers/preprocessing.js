380faba306ca5fb628d87f1c09be36aaf1695a66
StiefelJackal <stiefel@jackalworkz.net>
2024-11-18 22:16:34 -0500

    docs: add typedefs for session and world info

--- helpers/preprocessing.js
+++ helpers/preprocessing.js
@@ -1,11 +1,23 @@
 import DOMPurify from 'isomorphic-dompurify';
 
+/**
+ * Converts the world or session name to its HTML equivalent.
+ * 
+ * @param {string} name The name of the world or session to sanitize.
+ * @returns The processed world or session name that was sanitized.
+ */
 function preProcessName(name) {
     const start = /<color="?(.+?)"?>/gi;
     const end = /<\/color>/gi
     return name.replace(start, "<span style=\"color: $1;\">").replace(end, "</span>");
 }
 
+/**
+ * Preprocesses the session information returned from SkyFrost to a format suitable for viewing.
+ * 
+ * @param {SessionInfo} json The session object from SkyFrost to transform.
+ * @returns The transformed session object for viewing.
+ */
 function preProcessSession(json) {
     if (!json.thumbnailUrl || json.thumbnailUrl === "")
     {
@@ -19,6 +31,12 @@ function preProcessSession(json) {
     return json;
 }
 
+/**
+ * Preprocesses the world information return from SkyFrost to a format suitable for viewing.
+ * 
+ * @param {WorldInfo} json The world object from SkyFrost to transform.
+ * @returns The preprocessed world information for viewing.
+ */
 function preProcessWorld(json) {
     if (json.thumbnailUri) {
         json.thumbnailUri = json.thumbnailUri.replace("resdb:///", "https://assets.resonite.com/").replace(".webp", "");
@@ -29,6 +47,14 @@ function preProcessWorld(json) {
     return json;
 }
 
+/**
+ * Preprocesses the world or session information returned from SkyFrost
+ * to make it suitable for Web viewing.
+ * 
+ * @param {BaseWorldSessionInfo} json The world or session object from SkyFrost to transform.
+ * @param {('world'|'session')} type The type of information this is whether it is a world or session.
+ * @returns The preprocessed world or session object for viewing.
+ */
 export function preProcess(json, type) {
 
     // ensure page title
